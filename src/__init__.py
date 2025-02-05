b481249d1c1f34d8f16e451fa98d5ba712ac67d5
PrinceDobariya0710 <99081875+PrinceDobariya0710@users.noreply.github.com>
2025-02-06 00:01:00 +0530

    CRUD with neon db

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,15 @@
 # Inside main.py title
 from fastapi import FastAPI
 from src.books.routes import book_router
+from contextlib import asynccontextmanager
+from src.db.main import init_db
+
+@asynccontextmanager
+async def life_span(app:FastAPI):
+    print(f"server is starting...")
+    await init_db()
+    yield
+    print(f"server has been stopped...")
 
 version = 'v1'
 
@@ -8,6 +17,7 @@ app = FastAPI(
     title='Bookly',
     description='A RESTful API for a book review web service',
     version=version,
+    lifespan=life_span
 )
 
 app.include_router(book_router,prefix=f"/api/{version}/books", tags=['books'])
